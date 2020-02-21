29005f861149be095a308d063a95488d8e58841d
Kostiantyn Smyrnov <kostysh@gmail.com>
2020-02-21 05:16:10 +0200

    tests for resolver

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-const axios = require('axios');
+// const axios = require('axios');
+const Ajv = require('ajv');
 const expect = require('./utils/expect');
 const { createContract } = require('./utils/contracts');
 const { makeHash } = require('./utils/document');
-const OrganizationSchema = require('../build/Organization.json');// should be updated after wt-contracts package will be fixed
+const didDocumentSchema = require('../assets/did.json');
+const { OrgIdContract } = require('@windingtree/org.id');
 
 // ORG.ID resolver class
 class OrgIdResolver {
@@ -11,52 +13,72 @@ class OrgIdResolver {
         expect.all(options, {
             web3: {
                 type: 'object'
+            },
+            orgId: {
+                type: 'address'
             }
         });
 
-        this.web3 = web3;
+        this.web3 = options.web3;
+        this.orgId = options.orgId;
         this.methodName = 'orgid';
         this.fetchMethods = {};
+        this.reset();
+    }
+
+    reset() {
+        this.validator = new Ajv();
         this.resolutionStart = null;
         this.resultTemplate = {
             didDocument: {},
+            errors: [],
             resolverMetadata: {
-                driverId: 'did:orgid',
                 retrieved: null,
                 duration: null
             }
         };
+        this.result = Object.assign({}, this.resultTemplate);
     }
 
     async resolve(did) {
         expect.all({ did }, {
-            web3: {
+            did: {
+                type: 'string'
+            }
+        });
+        
+        this.reset();
+        this.resolutionStart = Date.now();
+        const id = await this.validateDidSyntax(did);
+        const didDocument = await this.getDidDocumentUri(id);
+        await this.validateDidDocument(didDocument);
+        await this.verifyTrustRecords(didDocument);
+
+        // Prepare result
+        this.result.didDocument = didDocument;
+        this.result.resolverMetadata.retrieved = new Date().toISOString();
+        this.result.resolverMetadata.duration = Date.now() - this.resolutionStart;
+
+        return this.result;
+    }
+
+    async fetchDidDocumentByUri(uri) {
+        expect.all({ uri }, {
+            uri: {
                 type: 'string'
             }
         });
 
-        this.resolutionStart = Date.now();
-        const id = this.validateDidSyntax(did);
-        const didDocument = await this.getDidDocumentUri(id);
-        this.validateDidDocument(didDocument);
-        this.verifyTrustRecords(didDocument);
-
-        // Prepare result
-        const result = Object.assign({}, this.resultTemplate);
-        result.didDocument = didDocument;
-        result.resolverMetadata.retrieved = new Date().toISOString();
-        result.resolverMetadata.duration = Date.now() - this.resolutionStart;
-
-        return result;
-    }
-
-    async fetchDidDocumentByUri(uri) {
         let fetch;
 
-        for (const f of this.fetchMethods) {
+        if (Object.keys(this.fetchMethods).length === 0) {
+            throw new Error('At least one fetching method should be registered');
+        }
 
-            if (RegExp(f.pattern).test(uri)) {
-                fetch = f.fetch;
+        for (const f in this.fetchMethods) {
+
+            if (RegExp(this.fetchMethods[f].pattern).test(uri)) {
+                fetch = this.fetchMethods[f].fetch;
                 break;
             }
         }
@@ -67,32 +89,60 @@ class OrgIdResolver {
             );
         }
 
-        return await fetch(uri);
+        const document = await fetch(uri);
+
+        if (!document) {
+            throw new Error('DID document not found by the given uri');
+        }
+
+        return  document;
     }
 
-    validateDidSyntax(did) {
+    async validateDidSyntax(did) {
+        expect.all({ did }, {
+            did: {
+                type: 'string'
+            }
+        });
+
         const parts = did.split(':');
 
         if (parts[0] !== 'did') {
-            throw new Error('Invalid DID prefix');
+            throw new Error(`Invalid DID prefix: ${parts[0]}`);
         }
 
         if (parts[1] !== this.methodName) {
-            throw new Error('Unsupported DID method');
+            throw new Error(`Unsupported DID method: ${parts[1]}`);
         }
 
         // Split paths, method parameters and queries
         const subParts = parts[2].split(/(?:#|;|\?)/);
 
-        if (!new RegExp('^0x[a-fA-F0-9]{40}$').test(subParts[0])) {
-            throw new Error('Invalid method specific id');
+        if (!new RegExp('^0x[a-fA-F0-9]{64}$').test(subParts[0])) {
+            throw new Error(`Invalid method specific id: ${subParts[0]}`);
         }
 
         return subParts[0];
     }
 
-    validateDidDocument(didDocument) {
+    async validateDidDocument(didDocument) {
+        expect.all({ didDocument }, {
+            didDocument: {
+                type: 'object'
+            }
+        });
 
+        const result = this.validator.validate(didDocumentSchema, didDocument);
+
+        if (this.validator.errors !== null) {
+
+            this.result.errors = [
+                ...this.result.errors,
+                ...this.validator.errors
+            ];
+        }
+
+        return result;
     }
 
     async verifyTrustRecords(didDocument) {
@@ -100,20 +150,27 @@ class OrgIdResolver {
     }
 
     async getDidDocumentUri(id) {
-        const contract = createContract(OrganizationSchema, this.web3);
-        const organization = await contract.at(id);
-        const uri = await organization.methods['getOrgJsonUri()'].call();
-        const hash = await organization.methods['getOrgJsonHash()'].call();
-        const didDocument = await this.fetchDidDocumentByUri(uri);
+        const contract = createContract(OrgIdContract, this.web3);
+        const orgId = await contract.at(this.orgId);
+        const organization = await orgId.methods['getOrganization(bytes32)'].call(id);
+        const didDocument = await this.fetchDidDocumentByUri(organization.orgJsonUri);
 
-        if (makeHash(JSON.stringify(didDocument)) !== hash) {
+        if (makeHash(didDocument, this.web3) !== organization.orgJsonHash) {
             throw new Error('Invalid DID Document hash');
         }
 
-        return didDocument;
+        const didObject = JSON.parse(didDocument);
+
+        if (`did:${this.methodName}:${id}` !== didObject.id) {
+            throw new Error(
+                `Invalid DID Document id. Expected to be: ${id}, but actual is: ${didObject.id}`
+            );
+        }
+
+        return didObject;
     }
 
-    registerFetchMethod(methodConfig = {}) {
+    async registerFetchMethod(methodConfig = {}) {
         expect.all(methodConfig, {
             name: {
                 type: 'string'
